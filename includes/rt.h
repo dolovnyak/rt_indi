8a826b4e608bd85a2f95fe2336d67cb49c5bbaca
GodFlight <prokushevmaksim@gmail.com>
2019-10-02 21:01:24 +0300

    norm fix include

--- includes/rt.h
+++ includes/rt.h
@@ -13,39 +13,38 @@
 #ifndef RT_H
 # define RT_H
 
-#ifndef OPENCL___
-# include "mlx.h"
-# include "libft.h"
-# include "libjtoc.h"
-# include "libcl.h"
-# include "get_next_line.h"
-# include <math.h>
-# include <stdlib.h>
-# include <fcntl.h>
-# include <stdio.h>
-# include <time.h>
-# include <OpenCL/cl.h>
-#endif
+# ifndef OPENCL___
+#  include "mlx.h"
+#  include "libft.h"
+#  include "libjtoc.h"
+#  include "libcl.h"
+#  include "get_next_line.h"
+#  include <math.h>
+#  include <stdlib.h>
+#  include <fcntl.h>
+#  include <stdio.h>
+#  include <time.h>
+#  include <OpenCL/cl.h>
+# endif
 
 # define WIDTH		1280
 # define HEIGHT		1024
 
-#ifndef OPENCL___
-#define SPEED1 0.5f
-#define SPEED2 (float)(M_PI / 90)
-#define SPEED3 1.f
-#define SPEED4 1.f
-#define CHANNEL_NUM 3
-#else
-#define MAX_DIST	10000.f
-#define SIGMA		3
-#endif
+# ifndef OPENCL___
+#  define SPEED1 0.5f
+#  define SPEED2 (float)(M_PI / 90)
+#  define SPEED3 1.f
+#  define SPEED4 1.f
+#  define CHANNEL_NUM 3
+# else
+#  define MAX_DIST	10000.f
+#  define SIGMA		3
+# endif
 
+# define PHONG		(1 << 0)
+# define PATH_TRACE	(1 << 1)
 
-#define PHONG		(1 << 0)
-#define PATH_TRACE	(1 << 1)
-
-enum				e_object_type
+enum					e_object_type
 {
 	o_sphere = 1,
 	o_plane,
@@ -56,41 +55,49 @@ enum				e_object_type
 	o_sqr,
 };
 
-typedef struct		s_light
+typedef struct			s_light
 {
-#ifndef OPENCL___
-	cl_float3		center;
-	cl_float		intens;
-#else
-	float3			center;
-	float			intens;
-#endif
-}					t_light;
+# ifndef OPENCL___
 
-typedef struct		s_material
-{
-#ifndef OPENCL___
-	cl_float3		diffuse_color;
-	cl_float2		al;
-	float			sp_ex;
-	float 			refraction;
-	float 			reflection;
-	cl_float3		emission;
-	int				texture_id;
-#else
-    float3			diffuse_color;
-	float2			al;
-	float			sp_ex;
-	float 			refraction;
-	float 			reflection;
-	float3			emission;
-	int				texture_id;
-#endif
-}					t_material;
+	cl_float3			center;
+	cl_float			intens;
+# else
 
-typedef struct		s_object
+	float3				center;
+	float				intens;
+
+# endif
+
+}						t_light;
+
+typedef struct			s_material
 {
-#ifndef	OPENCL___
+# ifndef OPENCL___
+
+	cl_float3			diffuse_color;
+	cl_float2			al;
+	float				sp_ex;
+	float				refraction;
+	float				reflection;
+	cl_float3			emission;
+	int					texture_id;
+# else
+
+	float3				diffuse_color;
+	float2				al;
+	float				sp_ex;
+	float				refraction;
+	float				reflection;
+	float3				emission;
+	int					texture_id;
+# endif
+
+}						t_material;
+
+typedef struct			s_object
+{
+# ifndef OPENCL___
+
 	int					type;
 	cl_float3			center;
 	cl_float3			vector;
@@ -102,7 +109,8 @@ typedef struct		s_object
 	cl_float3			d;
 	enum e_object_type	e_type;
 	t_material			mat;
-#else
+# else
+
 	int					type;
 	float3				center;
 	float3				vector;
@@ -114,164 +122,172 @@ typedef struct		s_object
 	float3				d;
 	enum e_object_type	e_type;
 	t_material			mat;
-#endif
-}					t_object;
+# endif
 
-typedef struct		s_screen
+}						t_object;
+
+typedef struct			s_screen
 {
-#ifndef OPENCL___
-	cl_float3		v1;
-	cl_float3		v2;
-	cl_float3		center;
-	cl_float		brightness;
-	cl_int			fsaa_n;
-	cl_int8			effects;
-	cl_int			params;
-	cl_int			samples;
-#else
-	float3			v1;
-	float3			v2;
-	float3			center;
-	float			brightness;
-	int				fsaa_n;
-	int8			effects;
-	int				params;
-	int				samples;
-#endif
-}					t_screen;
+# ifndef OPENCL___
 
-typedef struct		s_cam
+	cl_float3			v1;
+	cl_float3			v2;
+	cl_float3			center;
+	cl_float			brightness;
+	cl_int				fsaa_n;
+	cl_int8				effects;
+	cl_int				params;
+	cl_int				samples;
+# else
+
+	float3				v1;
+	float3				v2;
+	float3				center;
+	float				brightness;
+	int					fsaa_n;
+	int8				effects;
+	int					params;
+	int					samples;
+# endif
+
+}						t_screen;
+
+typedef struct			s_cam
 {
-#ifndef OPENCL___
-	cl_float3		center;
-	cl_float		alpha;
-	cl_float		betta;
-#else
-	float3		center;
-	float		alpha;
-	float		betta;
-#endif
-}					t_cam;
+# ifndef OPENCL___
 
-typedef struct		s_counter
+	cl_float3			center;
+	cl_float			alpha;
+	cl_float			betta;
+# else
+
+	float3				center;
+	float				alpha;
+	float				betta;
+# endif
+
+}						t_cam;
+
+typedef struct			s_counter
 {
-#ifndef OPENCL___
-	cl_int			l;
-	cl_int			all_obj;
-#else
-	int			l;
-	int			all_obj;
-#endif
-}					t_counter;
+# ifndef OPENCL___
 
-#ifndef OPENCL___
-typedef struct		s_mouse
+	cl_int				l;
+	cl_int				all_obj;
+# else
+
+	int					l;
+	int					all_obj;
+# endif
+
+}						t_counter;
+
+# ifndef OPENCL___
+
+typedef struct			s_mouse
 {
-	int				r;
-	int				l;
-	float			alpha;
-	float			betta;
-	int				x;
-	int				y;
-	cl_float3		center;
-}					t_mouse;
+	int					r;
+	int					l;
+	float				alpha;
+	float				betta;
+	int					x;
+	int					y;
+	cl_float3			center;
+}						t_mouse;
 
-typedef struct		s_img
+typedef struct			s_img
 {
-	void			*img_ptr;
-	int				*data;
-	int				size_l;
-	int				bpp;
-	int				endian;
-}					t_img;
+	void				*img_ptr;
+	int					*data;
+	int					size_l;
+	int					bpp;
+	int					endian;
+}						t_img;
 
-typedef struct 			s_obj_texture
+typedef struct			s_obj_texture
 {
 	char				**textures_path;
 	unsigned int		textures_count;
-	int 				w;
-	int 				h;
-	int 				*texture;
-	int 				bpp;
+	int					w;
+	int					h;
+	int					*texture;
+	int					bpp;
 	int					texture_w[100];
 	int					texture_h[100];
 	int					prev_texture_size[100];
 	size_t				texture_size;
 }						t_obj_texture;
 
-typedef struct 		s_gpu_mem
+typedef struct			s_gpu_mem
 {
-	cl_mem			cl_texture;
-	cl_mem			cl_texture_w;
-	cl_mem			cl_texture_h;
-	cl_mem			cl_prev_texture_size;
-	cl_mem			cl_img_buffer;
-	cl_mem			cl_aux_buffer;
-	cl_mem			cl_light_buffer;
-	cl_mem			cl_obj_buffer;
-	cl_mem			cl_counter_buffer;
-}					t_gpu_mem;
+	cl_mem				cl_texture;
+	cl_mem				cl_texture_w;
+	cl_mem				cl_texture_h;
+	cl_mem				cl_prev_texture_size;
+	cl_mem				cl_img_buffer;
+	cl_mem				cl_aux_buffer;
+	cl_mem				cl_light_buffer;
+	cl_mem				cl_obj_buffer;
+	cl_mem				cl_counter_buffer;
+}						t_gpu_mem;
 
-typedef struct		s_rt
+typedef struct			s_rt
 {
-	cl_float		ambient;
-	void			*mlx_ptr;
-	void			*win;
-	t_img			img;
-	t_cl			*cl;
-	t_screen		screen;
-	t_cam			cam;
-	t_counter		counter;
-	t_object		*obj;
-	t_light			*light;
-	t_mouse			mouse;
-	t_obj_texture	*texture;
-	t_gpu_mem		*gpu_mem;
-}					t_rt;
+	cl_float			ambient;
+	void				*mlx_ptr;
+	void				*win;
+	t_img				img;
+	t_cl				*cl;
+	t_screen			screen;
+	t_cam				cam;
+	t_counter			counter;
+	t_object			*obj;
+	t_light				*light;
+	t_mouse				mouse;
+	t_obj_texture		*texture;
+	t_gpu_mem			*gpu_mem;
+}						t_rt;
 
-int					check_key(int keycode, t_rt *rt);
-int					ft_esc(t_rt *rt);
-int					ft_move_x(int keycode, t_rt *rt);
-int					ft_move_y(int keycode, t_rt *rt);
-int					ft_move_alpha(int keycode, t_rt *rt);
-int					ft_move_betta(int keycode, t_rt *rt);
-int					ft_move_z(int keycode, t_rt *rt);
+int						check_key(int keycode, t_rt *rt);
+int						ft_esc(t_rt *rt);
+int						ft_move_x(int keycode, t_rt *rt);
+int						ft_move_y(int keycode, t_rt *rt);
+int						ft_move_alpha(int keycode, t_rt *rt);
+int						ft_move_betta(int keycode, t_rt *rt);
+int						ft_move_z(int keycode, t_rt *rt);
+int						mouse_press(int button, int x, int y, t_rt *rt);
+int						mouse_move(int x, int y, t_rt *rt);
+int						mouse_release(int button, int x, int y, t_rt *rt);
+int						ft_move_mouse(t_rt *rt, int button, int x, int y);
+int						ft_mouse_alpha_betta(t_rt *rt, int x, int y);
+int						ft_mouse_x_y(t_rt *rt, int x, int y);
+cl_float3				cl_sum(cl_float3 v1, cl_float3 v2);
+cl_float3				cl_minus(cl_float3 v1, cl_float3 v2);
+float					cl_length(cl_float3 v);
+cl_float3				cl_mult_n(cl_float3 v1, float n);
+void					cl_to_norm(cl_float3 *v);
+cl_float3				cl_normalize(cl_float3 v1);
+cl_float3				cl_cross(cl_float3 v1, cl_float3 v2);
+float					cl_dot(cl_float3 v1, cl_float3 v2);
+float					cl_angle(cl_float3 v1, cl_float3 v2);
+void					draw_picture(t_rt *rt);
+int						cl_worker(t_rt *rt);
+cl_device_id			create_device();
+cl_program				build_program(cl_context ctx, cl_device_id dev,
+											const char *filename);
+int						read_map(char *name, t_rt *rt);
+cl_float3				spherical(float phi, float tetta);
+cl_float3				spher_norm(cl_float3 v);
+void					calc_screen(t_screen *screen, t_cam *cam);
+void					get_textures(t_rt *rt, char **texture_file,
+												int number_of_texture);
+void					find_textures_size(t_rt *rt, char **texture_file,
+												int number_of_texture);
+void					fill_gpu_mem(t_rt *rt);
+void					release_gpu_mem(t_rt *rt);
+int						gauss_blur_gpu_init(t_rt *rt, cl_kernel *kernel,
+											cl_mem screen_buffer);
 
-
-int					mouse_press(int button, int x, int y, t_rt *rt);
-int					mouse_move(int x, int y, t_rt *rt);
-int					mouse_release(int button, int x, int y, t_rt *rt);
-int					ft_move_mouse(t_rt *rt, int button, int x, int y);
-int					ft_mouse_alpha_betta(t_rt *rt, int x, int y);
-int					ft_mouse_x_y(t_rt *rt, int x, int y);
-
-cl_float3			cl_sum(cl_float3 v1, cl_float3 v2);
-cl_float3			cl_minus(cl_float3 v1, cl_float3 v2);
-float				cl_length(cl_float3 v);
-cl_float3			cl_mult_n(cl_float3 v1, float n);
-void				cl_to_norm(cl_float3 *v);
-cl_float3			cl_normalize(cl_float3 v1);
-cl_float3			cl_cross(cl_float3 v1, cl_float3 v2);
-float				cl_dot(cl_float3 v1, cl_float3 v2);
-float				cl_angle(cl_float3 v1, cl_float3 v2);
-
-void 				draw_picture(t_rt *rt);
-int					cl_worker(t_rt *rt);
-cl_device_id		create_device();
-cl_program			build_program(cl_context ctx, cl_device_id dev, const char* filename);
-int					read_map(char *name, t_rt *rt);
-
-
-cl_float3			spherical(float phi, float tetta);
-cl_float3			spher_norm(cl_float3 v);
-void				calc_screen(t_screen *screen, t_cam *cam);
-
-void				get_textures(t_rt *rt, char **texture_file, int number_of_texture);
-void				find_textures_size(t_rt *rt, char **texture_file, int number_of_texture);
-void				fill_gpu_mem(t_rt *rt);
-void				release_gpu_mem(t_rt *rt);
-int					gauss_blur_gpu_init(t_rt *rt, cl_kernel *kernel,
-								   cl_mem screen_buffer);
-#endif
+# endif
 
 #endif
--- includes/rt_jtoc.h
+++ includes/rt_jtoc.h
@@ -1,11 +1,11 @@
 
-#ifndef	RT_JTOC_H
-#define RT_JTOC_H
+#ifndef RT_JTOC_H
+# define RT_JTOC_H
 
 extern char		*g_err_str;
 
 int			rt_jtoc_sdl_log_error(const char *p, const int id);
-int 		rt_jtoc_mouse_setup(t_rt *rt, const char *json);
+int			rt_jtoc_mouse_setup(t_rt *rt, const char *json);
 int			rt_jtoc_textures_setup(t_rt *rt, const char *json);
 int			rt_jtoc_scene_setup(t_rt *rt, const char *json);
 int			rt_jtoc_get_float2(cl_float2 *vec, t_jnode *n);
@@ -13,11 +13,11 @@ int			rt_jtoc_get_float3(cl_float3 *vec, t_jnode *n);
 int			rt_jtoc_get_float4(cl_float4 *vec, t_jnode *n);
 int			rt_jtoc_get_camera(t_cam *camera, t_jnode *n);
 int			rt_jtoc_get_objects_num_in_arr(unsigned int *objects_num,
-											  t_jnode *n);
+											t_jnode *n);
 int			rt_jtoc_get_objects(t_rt *rt, t_jnode *n,
-								   t_obj_texture *texture);
+									t_obj_texture *texture);
 int			rt_jtoc_get_object_texture(t_object *obj, t_obj_texture *texture,
-									  t_jnode *n);
+										t_jnode *n);
 int			rt_jtoc_get_object_type(t_object *obj, t_jnode *n);
 int			rt_jtoc_get_phong_param(t_object *obj, t_jnode *n);
 int			rt_jtoc_get_effects(t_rt *rt, t_jnode *n);
