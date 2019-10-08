f80409c442dd3f4d04d394c1a4f0c97f6aa31d80
GodFlight <prokushevmaksim@gmail.com>
2019-10-08 23:43:57 +0300

    fix mouse

--- srcs/check_mouse_0.c
+++ srcs/check_mouse_0.c
@@ -22,10 +22,17 @@ int		mouse_press(int button, int x, int y, t_rt *rt)
 {
 	rt->screen.fsaa_n = 0;
 	rt->screen.params = PHONG;
+
+	if (rt->mouse.y < 0)
+	{
+		rt->mouse.y = 0;
+		rt->mouse.l = 0;
+		rt->mouse.r = 0;
+	}
 	if (((button == 4) || (button == 5))
 		&& (rt->mouse.r == 0 && rt->mouse.l == 0)
 		&& ft_move_mouse(rt, button, x, y))
-		rerender(rt);
+			rerender(rt);
 	else if ((button == 1) && (rt->mouse.l == 0) && (rt->mouse.r == 0))
 	{
 		rt->mouse.alpha = rt->cam.alpha;
@@ -33,6 +40,7 @@ int		mouse_press(int button, int x, int y, t_rt *rt)
 		rt->mouse.x = x;
 		rt->mouse.y = y;
 		rt->mouse.l = 1;
+
 	}
 	else if ((button == 2) && (rt->mouse.l == 0) && (rt->mouse.r == 0))
 	{
@@ -48,6 +56,9 @@ int		mouse_press(int button, int x, int y, t_rt *rt)
 
 int		mouse_move(int x, int y, t_rt *rt)
 {
+	if (rt->mouse.y < 0)
+		return (0);
+
 	if ((rt->mouse.l == 1) && (rt->mouse.r == 0))
 		ft_mouse_alpha_betta(rt, x, y);
 	else if (rt->mouse.r == 1)
